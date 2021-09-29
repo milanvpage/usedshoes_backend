21bfd094ace9c162e6a43c8acb0db90662a0b9fc
Milan <milanvpage@gmail.com>
2021-09-29 13:14:13 -0700

    create creat method inside of Comments Controller so i can create comments on my frontend

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,15 @@ class CommentsController < ApplicationController
         comments = Comment.all
         render json: comments
     end
+
+    def create
+        comment = Comment.new(comment_params)
+    end
+
+
+    private
+
+    def comment_params
+        
+    end
 end
