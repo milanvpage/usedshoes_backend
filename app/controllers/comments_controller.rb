d0c70f29d78b0fc67226bc23b3860ca5ecf213ef
Milan <milanvpage@gmail.com>
2021-10-01 10:17:54 -0700

    created if statement inside create method inside CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,12 @@ class CommentsController < ApplicationController
 
     def create
         comment = Comment.new(comment_params)
+        #byebug
+        if comment.save
+            render json: comment
+        else
+            render json: {errors: "Couldn't be Saved"}
+        end
     end
 
 
